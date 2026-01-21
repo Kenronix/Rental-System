8f4b6a31ffed152a8b960550d77329c2040e71d8
Kenronix <kennethjamesbatuhan@gmail.com>
2026-01-21 17:58:05 +0800

    landing page

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-      <h1>Laravel + Vue is working 🚀</h1>
-    </div>
-  </template>
-  
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+// App.vue is the root component
+// It will render whatever route is active via router-view
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
