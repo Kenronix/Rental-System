32846bc9b973b06ede61d744e669f1c666fb0c3e
Kenronix <kennethjamesbatuhan@gmail.com>
2026-01-23 11:33:09 +0800

    Add landlord reports view

---
+++ resources/js/views/LandlordReports.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="dashboard-layout">
+    <Sidebar />
+    <div class="main-content">
+      <h1>Reports</h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from '../components/layout/Sidebar.vue'
+</script>
+
+<style scoped>
+.dashboard-layout {
+  display: flex;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex: 1;
+  margin-left: 300px;
+  padding: 40px;
+  background: #F0F0F0;
+  min-height: 100vh;
+}
+</style>
